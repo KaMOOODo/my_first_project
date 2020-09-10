be8b77b8cb35f8197a7b465dbf5f92ab6450704f
kamooodo <gomelsash@gmail.com>
2020-09-10 21:54:34 +0300

    First branch created

--- main.py
+++ main.py
@@ -13,4 +13,45 @@ print('It is first branch')
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import pandas
+df = pandas.read_csv('/home/user/Загрузки/titanic.csv')
+
+print(df.columns)
+print(df.shape[0])
+
+def Mans_on_board():return df[df['Sex'] == 'male'].shape[0]
+print(Mans_on_board())
+
+def alive_proc():
+    alive = df[df['Survived'] == 1].shape[0]
+    all = df.shape[0]
+    return alive*100/all
+print(alive_proc())
+
+def mf():
+    male = df[df['Sex'] == 'male'].shape[0]
+    female = df[df['Sex'] == 'female'].shape[0]
+    if male > female:
+        return ('Male more')
+    else:
+        return ('Female more')
+
+print(mf())
+
+def surv_man():
+    return (df[(df['Sex'] == 'male') & (df['Survived'] == 1)].shape[0]/df[df['Survived'] == 1].shape[0])*100
+
+print(surv_man())
+
+def surv_class():
+    var = {
+        'First': (df[(df['Pclass'] == 1) & (df['Survived'] == 1)].shape[0] / df[df['Survived'] == 1].shape[0]) * 100,
+        'Second': (df[(df['Pclass'] == 2) & (df['Survived'] == 1)].shape[0] / df[df['Survived'] == 1].shape[0]) * 100,
+        'Third': (df[(df['Pclass'] == 3) & (df['Survived'] == 1)].shape[0] / df[df['Survived'] == 1].shape[0]) * 100
+    }
+    # min = min(var.values())
+    min(var.values()) in var
+    return (min(var()))
+
+print(surv_class())
